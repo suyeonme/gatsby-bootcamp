8f540fb755547cef7ba59b75dc28a3605c22f75d
Suyeon Kang <tndusrkd92@gmail.com>
2020-09-13 19:01:18 +0900

    Create pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return <h1>This is home page</h1>
 }
+
+export default IndexPage
